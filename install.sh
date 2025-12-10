4721696ed653fe85dc0df23ef8d009109f791bed
minstrike520 <minstrike520@gmail.com>
2025-12-10 03:43:02 +0000

    install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,36 @@
+#!/bin/bash
+
 cd $(dirname "$0")/stow
 
-if [[ "$#" -eq 1 ]]; then
-  echo this path
-  stow --verbose -d ./ -t ~/ $1
+GETOPT_ARGS=`getopt --options f --long force -n "$0" -- "$@"`
+
+eval set -- "${GETOPT_ARGS}"
+
+stow_flags="--verbose"
+
+while true; do
+  case "$1" in
+    -f|--force)
+      stow_flags="$stow_flags --override .* --adopt"
+      echo "Are you sure to override the existing files in the file system (if any)?"
+      read
+      shift
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo $0: Unexpected Error
+      exit 1
+      ;;
+  esac
+done
+
+if [[ $# -eq 0 ]]; then
+  stow -d ./ -t $HOME/ */ $stow_flags
 else
-  echo that path
-  stow -d ./ -t ~/ */
+  for target in "$@"; do
+    stow -d ./ -t $HOME/ $target $stow_flags
+  done
 fi
