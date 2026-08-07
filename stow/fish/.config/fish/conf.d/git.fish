3015f44477b9ae6e2a3a53adb6168c402f43a8bc
minstrike520 <minstrike520@gmail.com>
2026-08-07 05:34:43 +0000

    auto: sync (2026-08-07 05:34:43) [aspc]

--- stow/fish/.config/fish/conf.d/git.fish
+++ stow/fish/.config/fish/conf.d/git.fish
@@ -4,5 +4,5 @@
 
 set GIT_PAGER ""
 
-alias gitlog='git log --oneline --decorate --all --graph'
+alias gitlog='git log --oneline --decorate --all --graph --color=always | head -n 10'
 alias groot='cd "$(git rev-parse --show-toplevel)"'
